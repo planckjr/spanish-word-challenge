aa24fcd133a0d317c979bb8abc094028e63f8110
Neo Vidal <planckjr@gmail.com>
2025-02-17 20:35:46 -0300

    Create words.js

---
+++ words.js
@@ -0,0 +1,19 @@
+window.spanishWords = [
+    "el", "la", "de", "que", "y", "a", "en", "un", "ser", "se",
+    "no", "haber", "por", "con", "su", "para", "como", "estar",
+    "tener", "le", "lo", "todo", "pero", "más", "hacer", "o", "poder",
+    "decir", "este", "ir", "otro", "ese", "la", "sí", "me", "ya",
+    "ver", "porque", "dar", "cuando", "muy", "sin", "vez", "mucho",
+    "saber", "qué", "sobre", "mi", "alguno", "mismo", "yo", "también",
+    "hasta", "año", "dos", "querer", "entre", "así", "primero", "desde",
+    "grande", "eso", "ni", "nos", "llegar", "pasar", "tiempo", "ella",
+    "sí", "día", "uno", "bien", "poco", "deber", "entonces", "poner",
+    "cosa", "tanto", "hombre", "parecer", "nuestro", "tan", "donde",
+    "ahora", "parte", "después", "vida", "quedar", "siempre", "creer",
+    "hablar", "llevar", "dejar", "nada", "cada", "seguir", "menos",
+    "nuevo", "encontrar", "algo", "casa", "aquel", "momento", "forma",
+    "tres", "quién", "volver", "caso", "allí", "contra", "hecho",
+    "bueno", "mirar", "trabajo", "casi", "estar", "poder", "hora",
+    "ser", "lugar", "mujer", "niño", "gente", "manera", "tipo", "cualquiera",
+    "mayor", "mundo", "aunque", "historia", "modo", "nombre", "fin", "país"
+];
